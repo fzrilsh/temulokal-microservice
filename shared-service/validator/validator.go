df03e632aec8a11147a006a738350364b869643f
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-19 15:35:02 +0700

    add validator tag name func with json tag support for struct fields

--- shared-service/validator/validator.go
+++ shared-service/validator/validator.go
@@ -1,6 +1,7 @@
 package validator
 
 import (
+	"reflect"
 	"strings"
 
 	"github.com/go-playground/validator/v10"
@@ -16,6 +17,13 @@ type ValidationErrorResponse struct {
 // Init initializes the validator instance
 func Init() {
 	validate = validator.New()
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
 }
 
 // ValidateStruct validates struct using go-playground/validator
@@ -29,7 +37,7 @@ func ValidateStruct(data interface{}) []ValidationErrorResponse {
 		var errors []ValidationErrorResponse
 		for _, e := range err.(validator.ValidationErrors) {
 			errors = append(errors, ValidationErrorResponse{
-				Field: strings.ToLower(e.Field()),
+				Field: e.Field(),
 				Tag:   e.Tag(),
 			})
 		}
