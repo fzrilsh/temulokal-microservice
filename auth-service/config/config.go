b1b7f6a8ed7196f9dea47ed4ac565b5436d82b86
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-19 13:40:06 +0700

    add configuration loading for smtp email service

--- auth-service/config/config.go
+++ auth-service/config/config.go
@@ -3,34 +3,54 @@ package config
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
+	// App
 	AppName   string
 	AppPort   string
 	AppEnv    string
 	AppOrigin string
-	DBHost    string
-	DBUser    string
-	DBPass    string
-	DBName    string
+
+	// Database
+	DBHost string
+	DBUser string
+	DBPass string
+	DBName string
+
+	// Email
+	SMTPHost     string
+	SMTPPort     int
+	SMTPPassword string
+	SMTPSender   string
+
+	// JWT
 	JWTSecret string
 }
 
 func Load() *Config {
 	_ = godotenv.Load()
+	SMTPPort, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
 
 	cfg := &Config{
 		AppName:   os.Getenv("APP_NAME"),
 		AppPort:   os.Getenv("APP_PORT"),
 		AppEnv:    os.Getenv("APP_ENV"),
 		AppOrigin: os.Getenv("APP_ORIGIN"),
-		DBHost:    os.Getenv("DB_HOST"),
-		DBUser:    os.Getenv("DB_USER"),
-		DBPass:    os.Getenv("DB_PASS"),
-		DBName:    os.Getenv("DB_NAME"),
+
+		DBHost: os.Getenv("DB_HOST"),
+		DBUser: os.Getenv("DB_USER"),
+		DBPass: os.Getenv("DB_PASS"),
+		DBName: os.Getenv("DB_NAME"),
+
+		SMTPHost:     os.Getenv("SMTP_HOST"),
+		SMTPPort:     SMTPPort,
+		SMTPPassword: os.Getenv("SMTP_PASSWORD"),
+		SMTPSender:   os.Getenv("SMTP_SENDER"),
+
 		JWTSecret: os.Getenv("JWT_SECRET"),
 	}
 
