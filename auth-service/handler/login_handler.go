d2c697a0fa3305386f1309ce9d86ce8433d945eb
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-19 13:26:31 +0700

    add login handler with authentication logic and JWT token generation

---
+++ auth-service/handler/login_handler.go
@@ -0,0 +1,51 @@
+package handler
+
+import (
+	"temulokal-microservice/auth-service/usecase"
+	"temulokal-microservice/auth-service/utils/jwt"
+	"temulokal-microservice/auth-service/utils/validator"
+	"temulokal-microservice/shared-service/response"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type LoginHandler struct {
+	usecase    *usecase.AuthUsecase
+	jwtManager *jwt.JWTManager
+}
+
+// constructor
+func NewLoginHandler(u *usecase.AuthUsecase, jwtManager *jwt.JWTManager) *LoginHandler {
+	return &LoginHandler{
+		usecase:    u,
+		jwtManager: jwtManager,
+	}
+}
+
+// POST /auth/login
+func (h *LoginHandler) Handler(c *fiber.Ctx) error {
+	var input usecase.LoginInput
+
+	if err := c.BodyParser(&input); err != nil {
+		return response.Error(c, fiber.StatusBadRequest, "Invalid request body", nil)
+	}
+
+	if err := validator.ValidateStruct(input); err != nil {
+		return response.Error(c, fiber.StatusUnprocessableEntity, "Invalid fields", err)
+	}
+
+	user, err := h.usecase.Login(input.Email, input.Password)
+	if err != nil {
+		return response.Error(c, fiber.StatusUnauthorized, "Invalid email or password", nil)
+	}
+
+	token, err := h.jwtManager.Generate(user.ID)
+	if err != nil {
+		return response.Error(c, fiber.StatusInternalServerError, "Something went wrong, please try again", nil)
+	}
+
+	return response.Success(c, fiber.StatusOK, "Logged in successfully", fiber.Map{
+		"token": token,
+		"user":  user,
+	})
+}
