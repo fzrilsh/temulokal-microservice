7d0cdd31e1b8ae1ac8e9e4ca7a8aa8e71f077734
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-19 13:25:43 +0700

    add server and routes setup for auth service with error handling and middleware

---
+++ auth-service/bootstrap/routes.go
@@ -0,0 +1,31 @@
+package bootstrap
+
+import (
+	"time"
+
+	"temulokal-microservice/auth-service/config"
+	"temulokal-microservice/auth-service/handler"
+	"temulokal-microservice/auth-service/repository"
+	"temulokal-microservice/auth-service/usecase"
+	"temulokal-microservice/auth-service/utils/database"
+	"temulokal-microservice/auth-service/utils/jwt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterRoutes(app *fiber.App) {
+	cfg := config.Load()
+	db := database.Connect(cfg)
+	jwtManager := jwt.NewJWTManager(cfg.AppEnv, cfg.JWTSecret, time.Hour*24*5)
+
+	emailRepo := repository.NewEmailRepository(cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.From)
+	userRepo := repository.NewUserRepository(db)
+	authUsecase := usecase.NewAuthUsecase(userRepo)
+
+	loginHandler := handler.NewLoginHandler(authUsecase, jwtManager)
+	registerHandler := handler.NewRegisterHandler(authUsecase, jwtManager, emailRepo)
+
+	auth := app.Group("/auth")
+	auth.Post("/login", loginHandler.Handler)
+	auth.Post("/register", registerHandler.Handler)
+}
