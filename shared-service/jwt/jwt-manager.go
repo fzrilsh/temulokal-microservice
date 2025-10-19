f9b81cbcba2863e20cf7ba0d600b466e469633f6
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-19 14:09:36 +0700

    add jwt manager and validator service

---
+++ shared-service/jwt/jwt-manager.go
@@ -0,0 +1,58 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTManager struct {
+	appEnv        string
+	secretKey     string
+	tokenDuration time.Duration
+}
+
+type UserClaims struct {
+	UserID uint `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func NewJWTManager(appEnv string, secretKey string, duration time.Duration) *JWTManager {
+	return &JWTManager{
+		appEnv:        appEnv,
+		secretKey:     secretKey,
+		tokenDuration: duration,
+	}
+}
+
+// Generate token from user ID
+func (j *JWTManager) Generate(userID uint) (string, error) {
+	claims := UserClaims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(j.tokenDuration)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(j.secretKey))
+}
+
+// Verify and parse token
+func (j *JWTManager) Verify(accessToken string) (*UserClaims, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &UserClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(j.secretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*UserClaims)
+	if !ok || !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
+}
