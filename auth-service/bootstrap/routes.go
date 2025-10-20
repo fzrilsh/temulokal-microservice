a9351937eb3680ab69271d550745cc97fe37514d
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-20 20:52:05 +0700

    update user repository initialization to include cache

--- auth-service/bootstrap/routes.go
+++ auth-service/bootstrap/routes.go
@@ -11,15 +11,18 @@ import (
 	"temulokal-microservice/shared-service/jwt"
 
 	"github.com/gofiber/fiber/v2"
+	gocache "github.com/patrickmn/go-cache"
 )
 
 func RegisterRoutes(app *fiber.App) {
 	cfg := config.Load()
+	cache := gocache.New(5*time.Minute, 10*time.Minute)
+
 	db := database.Connect(cfg)
 	jwtManager := jwt.NewJWTManager(cfg.JWTSecret, time.Hour*24*5)
 
 	emailRepo := repository.NewEmailRepository(cfg.SMTPHost, cfg.SMTPPort, cfg.SMTPSender, cfg.SMTPPassword)
-	userRepo := repository.NewUserRepository(db)
+	userRepo := repository.NewUserRepository(db, cache)
 	authUsecase := usecase.NewAuthUsecase(userRepo)
 
 	loginHandler := handler.NewLoginHandler(authUsecase, jwtManager)
