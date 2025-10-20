29d3867426a34d920bc36a1cee494a7163ed0a24
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-20 20:52:14 +0700

    add user caching with go-cache for findbyemail method

--- auth-service/repository/user_repository.go
+++ auth-service/repository/user_repository.go
@@ -2,11 +2,11 @@ package repository
 
 import (
 	"errors"
-
 	"temulokal-microservice/auth-service/model"
 	"temulokal-microservice/auth-service/utils/passwords"
 	"temulokal-microservice/shared-service/httpclient"
 
+	gocache "github.com/patrickmn/go-cache"
 	"gorm.io/gorm"
 )
 
@@ -17,25 +17,34 @@ type UserRepository interface {
 
 type userRepository struct {
 	db         *gorm.DB
+	cache      *gocache.Cache
 	HTTPClient *httpclient.Client
 }
 
 // Constructor
-func NewUserRepository(db *gorm.DB) UserRepository {
+func NewUserRepository(db *gorm.DB, cache *gocache.Cache) UserRepository {
 	return &userRepository{
 		db:         db,
+		cache:      cache,
 		HTTPClient: httpclient.New(),
 	}
 }
 
 func (r *userRepository) FindByEmail(email string) (*model.User, error) {
+	if cachedUser, found := r.cache.Get(email); found {
+		if user, ok := cachedUser.(model.User); ok {
+			return &user, nil
+		}
+	}
+
 	var user model.User
-	result := r.db.Where("email = ?", email).First(&user)
+	result := r.db.Model(&model.User{}).Where("email = ?", email).First(&user)
 
 	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 		return nil, errors.New("user not found")
 	}
 
+	r.cache.Set(email, user, gocache.DefaultExpiration)
 	return &user, nil
 }
 
