29583a0899d1ec7adbcbccda226542f1057d2fa0
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-20 09:45:24 +0700

    add configuration loading with environment variables for gateway service

---
+++ gateway/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppPort   string
+	JWTSecret string
+
+	// services
+	AuthServiceOrigin string
+}
+
+func Load() *Config {
+	_ = godotenv.Load()
+
+	cfg := &Config{
+		JWTSecret:         os.Getenv("JWT_SECRET"),
+		AppPort:           os.Getenv("APP_PORT"),
+		AuthServiceOrigin: os.Getenv("AUTH_SERVICE_ORIGIN"),
+	}
+
+	return cfg
+}
