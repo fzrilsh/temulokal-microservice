66aa489c427f9efbfad307fddc4938b40608a3bd
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-10-20 09:45:18 +0700

    add start command with proxy initialization

---
+++ gateway/cmd/start.go
@@ -0,0 +1,15 @@
+package cmd
+
+import (
+	"temulokal-microservice/gateway/bootstrap"
+
+	"github.com/spf13/cobra"
+)
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start gateway proxy",
+	Run: func(cmd *cobra.Command, args []string) {
+		bootstrap.StartProxy()
+	},
+}
