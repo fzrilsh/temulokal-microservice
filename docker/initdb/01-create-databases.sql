fc8b30ed3ec7e767e70bc66601162af878864593
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-11-12 22:24:43 +0700

    feat: create initial SQL script for database setup

---
+++ docker/initdb/01-create-databases.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS temulokal_auth_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS temulokal_umkm_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
